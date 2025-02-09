f24c166d0eb8f9c50a3356225e9e9f427471d5fb
mlmarcosluan <mlmarcosluan@gmail.com>
2025-02-09 11:11:39 -0300

    Para fins de estudos estou pegando as ultimas mensagens e salvando em um dicionário

--- elavador_info.py
+++ elavador_info.py
@@ -7,6 +7,7 @@ import asyncio
 import os
 from dotenv import load_dotenv
 import asyncio
+from datetime import datetime, timedelta, timezone
 
 # ================================================
 #        Pegar dados do arquivo .env
@@ -38,9 +39,6 @@ def dados_telegram():
 # ================================================
 
 def menu_principal():
-
-    
-
     print("================================================")
     print("Digite: ")
     print("1, para buscar mensagens novas.")
@@ -54,6 +52,58 @@ def menu_principal():
     limpar_tela()
 
     return (escolha)
+
+# ================================================
+#        Pesquisar novas mensagens
+# ================================================
+
+async def pesquisar_mensagens():
+    api_id, api_hash, phone_number, session_name, user_name_grupo = dados_telegram()
+            # Criar cliente do Telegram
+    client = TelegramClient(session_name, api_id, api_hash)
+            
+    try:
+        # Conectar ao Telegram
+        await client.start(phone=phone_number)
+        print("Conectado ao Telegram!")
+        
+        # Verificar se o usuário está autorizado
+        if not await client.is_user_authorized():
+            print("Usuário não autenticado. Iniciando autenticação...")
+            await client.send_code_request(phone_number)
+            codigo = input("Digite o código recebido no telefone: ")
+            await client.sign_in(phone=phone_number, code=codigo)
+        
+        print("Autenticação concluída com sucesso!")
+        
+        # Acessar o grupo
+        try:
+            group_entity = await client.get_entity(user_name_grupo)
+            print(f"Acessando grupo: {group_entity.title}")
+            
+            # Capturar as últimas mensagens do grupo
+            print("Capturando as últimas mensagens...")
+            mensagens = await client.get_messages(group_entity, limit=100)  # Limitar a 100 mensagens
+            
+            dicionario_mensagens = {}
+            for mensagem in mensagens:
+                # Ajustar data e hora para o brasil
+                data_utc = mensagem.date  # Data original em UTC
+                data_brasil = data_utc.astimezone(timezone(timedelta(hours=-3)))  # Ajuste para UTC-3
+
+                # Adicionar informações da mensagem no dicionario
+                dicionario_mensagens[data_brasil] = {"Andar": mensagem.message,"Ano": data_brasil.year, "Mes": data_brasil.month, "Dia": data_brasil.day, "Hora": data_brasil.hour, "Minuto": data_brasil.minute} 
+
+
+            input("Aperte enter para coninuar")
+        except Exception as e:
+            print(f"Erro ao acessar o grupo: {e}")
+    
+    finally:
+        # Desconectar o cliente
+        await client.disconnect()
+
+    return dicionario_mensagens
 
 # ================================================
 #        Função gerais
@@ -80,48 +130,13 @@ async def main():
             input("Aperte enter para continuar...")
             continue
         if escolha == 1: # Foi escolhido pesquisar novas mensagens
-
-            api_id, api_hash, phone_number, session_name, user_name_grupo = dados_telegram()
-            # Criar cliente do Telegram
-            client = TelegramClient(session_name, api_id, api_hash)
-            
-            try:
-                # Conectar ao Telegram
-                await client.start(phone=phone_number)
-                print("Conectado ao Telegram!")
-                
-                # Verificar se o usuário está autorizado
-                if not await client.is_user_authorized():
-                    print("Usuário não autenticado. Iniciando autenticação...")
-                    await client.send_code_request(phone_number)
-                    codigo = input("Digite o código recebido no telefone: ")
-                    await client.sign_in(phone=phone_number, code=codigo)
-                
-                print("Autenticação concluída com sucesso!")
-                
-                # Acessar o grupo
-                try:
-                    group_entity = await client.get_entity(user_name_grupo)
-                    print(f"Acessando grupo: {group_entity.title}")
-                    
-                    # Capturar as últimas mensagens do grupo
-                    print("Capturando as últimas mensagens...")
-                    messages = await client.get_messages(group_entity, limit=100)  # Limitar a 100 mensagens
-                    
-                    for message in messages:
-                        print(f"Mensagem: {message.message} | Data: {message.date}")
-
-                    input("Aperte enter para coninuar")
-                except Exception as e:
-                    print(f"Erro ao acessar o grupo: {e}")
-            
-            finally:
-                # Desconectar o cliente
-                await client.disconnect()
+            dicionario_mensagens = await pesquisar_mensagens()
+        
         elif escolha == 0:
             print("Encerrando programa.")
             break
-
+        
+        print("Final")
 # Executar o script
 if __name__ == "__main__":
     asyncio.run(main())
