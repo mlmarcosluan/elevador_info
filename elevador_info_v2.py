49d03fe20cbbe61b3a82db7bdafe5ac8e837da0f
mlmarcosluan <mlmarcosluan@gmail.com>
2025-02-13 08:11:23 -0300

    Adicionado a função conecta_telegram(), que faz a conecxão com o telegram e pega as ultimas mensagens

--- elevador_info_v2.py
+++ elevador_info_v2.py
@@ -27,7 +27,7 @@ from colorama import init, Fore, Style
 
 # ================================================
 #        Pegar dados do arquivo .env
-# ================================================
+
 def dados_api_telegram():
     # Caminho do arquivo .env
     caminho_env = "/home/marcos/VScode/dados.env"
@@ -48,6 +48,65 @@ def dados_api_telegram():
         return
 
     return api_id, api_hash, phone_number, session_name, user_name_grupo
+
+# ================================================
+
+# ================================================
+#       Conectar ao telegram
+async def conectar_telegram ():
+    api_id, api_hash, phone_number, session_name, user_name_grupo = dados_api_telegram()
+    # Criar cliente do Telegram
+    client = TelegramClient(session_name, api_id, api_hash)
+            
+    try:
+        # Conectar ao Telegram
+        await client.start(phone=phone_number)
+        print("Conectado ao Telegram!")
+        
+        # Verificar se o usuário está autorizado
+        if not await client.is_user_authorized():
+            print("Usuário não autenticado. Iniciando autenticação...")
+            await client.send_code_request(phone_number)
+            codigo = input("Digite o código recebido no telefone: ")
+            await client.sign_in(phone=phone_number, code=codigo)
+        
+        print("Autenticação concluída com sucesso!")
+        
+        # Acessar o grupo
+        try:
+            group_entity = await client.get_entity(user_name_grupo)
+            print(f"Acessando grupo: {group_entity.title}")
+            
+            # Capturar as últimas mensagens do grupo
+            print("Capturando as últimas mensagens...")
+            mensagens = await client.get_messages(group_entity, limit=100)  # Limitar a 100 mensagens
+            
+            dicionario_mensagens = {}
+            for mensagem in mensagens:
+                # Ajustar data e hora para o brasil
+                data_utc = mensagem.date  # Data original em UTC
+                data_brasil = data_utc.astimezone(timezone(timedelta(hours=-3)))  # Ajuste para UTC-3
+
+                # Adicionar informações da mensagem no dicionario
+                
+                dicionario_mensagens[data_brasil] = {"Andar": mensagem.message,"Ano": data_brasil.year, "Mês": data_brasil.month, "Dia": data_brasil.day, "Horas": data_brasil.hour, "Minutos": data_brasil.minute, "Segundos": data_brasil.second} 
+
+            ids_mensagens = dicionario_mensagens.keys()
+            lista_ids_mensagens = list(ids_mensagens)
+            
+        except Exception as e:
+            print(f"Erro ao acessar o grupo: {e}")
+    
+    finally:
+        # Desconectar o cliente
+        await client.disconnect()
+
+    return lista_ids_mensagens, dicionario_mensagens
+
+    
+            
+# ================================================
+
 # ================================================
 #        Funções gerais
 
@@ -83,10 +142,21 @@ def menu_principal():
         # Entrada do usuário
         try:
             escolha = int(input(Fore.MAGENTA + "Digite sua escolha: " + Fore.WHITE))
-            return escolha
+
         except ValueError:
             print(Fore.RED + "Erro: Por favor, digite um número válido.")
             input(Fore.YELLOW + "Pressione Enter para continuar...")
+
+        # Verifica a escolha
+        if escolha == 0:
+            return escolha
+        elif escolha == 1:
+            return escolha
+        else:
+            print(Fore.RED + "Digite uma opção válida...")
+            input(Fore.YELLOW + "Aperte enter para continuar...")
+        
+
 
 # ================================================
 
@@ -99,7 +169,15 @@ async def main():
 
     # Chama o menu principal
     escolha = menu_principal()
-    
+
+    if escolha == 0:
+        return
+    elif escolha == 1:
+        # Procurar dados do telegram
+        lista_ids_mensagens, dicionario_mensagens = await conectar_telegram()
+
+        # Conecta ao telgram e busca as mensagens
+
 
 # ================================================    
 
