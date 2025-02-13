930b2f69820ae1d6724d6248135b1d687618e729
mlmarcosluan <mlmarcosluan@gmail.com>
2025-02-13 06:55:22 -0300

    Adicionado a função dados_api_telegram(), que pega os dados necessarios para entrar na api do telegram que estão em um arquivo .env.

--- elevador_info_v2.py
+++ elevador_info_v2.py
@@ -1,7 +1,6 @@
 # ================================================
 #        Importar pacotes
 # ================================================
-
 # Importa a classe TelegramClient da biblioteca Telethon, que permite interagir com a API do Telegram.
 from telethon import TelegramClient
 
@@ -23,3 +22,26 @@ from dotenv import load_dotenv
 # Importa classes do módulo datetime (datetime, timedelta, timezone) para manipular datas, horários e fusos horários.
 from datetime import datetime, timedelta, timezone
 
+# ================================================
+#        Pegar dados do arquivo .env
+# ================================================
+def dados_api_telegram():
+    # Caminho do arquivo .env
+    caminho_env = "/home/marcos/VScode/dados.env"
+    
+    # Carregar variáveis do arquivo .env
+    load_dotenv(caminho_env)
+    
+    # Obter variáveis de ambiente
+    api_id = os.getenv("api_id")
+    api_hash = os.getenv("api_hash")
+    phone_number = os.getenv("phone_number")
+    session_name = os.getenv("session_name")
+    user_name_grupo = os.getenv("user_name_grupo")
+    
+    # Verificar se as variáveis foram carregadas corretamente
+    if not all([api_id, api_hash, phone_number, session_name, user_name_grupo]):
+        print("Erro: Variáveis de ambiente não encontradas. Verifique o arquivo .env.")
+        return
+
+    return api_id, api_hash, phone_number, session_name, user_name_grupo
